b63397b0f250c7890dac10f5b808f2212ca26c85
JiwoonKim <diana0501@naver.com>
2019-05-25 02:06:00 +0900

    6-3. make change 추가

---
+++ 6_dp_recursion/3_make_change.cpp
@@ -0,0 +1,27 @@
+/**
+ *  Calculate the number of ways to make change
+ * 
+ *  Time Complexity: O(
+ *  Space Complexity: O(
+ * 
+ *  * Stuck Point:
+ *     - could not figure how the relationship works
+ *     - could not figure how to structure dp
+ * 
+ *  ** Untangled Hints: 
+ * 
+ *  * What I learned:
+ * 
+ *    SO SO HARD...
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ *  calculate the number of ways to make change
+ */
+size_t changePossibilities(int amount, const vector<int>& denominations) {
+
+}
