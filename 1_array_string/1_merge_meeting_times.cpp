9dfe1c6de4d30f6e493f47f9fd7b1520b7d41258
JiwoonKim <diana0501@naver.com>
2019-05-27 00:59:38 +0900

    1-1. merge meeting times 필기 완료

--- 1_array_string/1_merge_meeting_times.cpp
+++ 1_array_string/1_merge_meeting_times.cpp
@@ -1,10 +1,41 @@
 /**
- *  R
+ *  Merge meeting ranges
  * 
- *  Time Complexity: O(n)
- *  Space Complexity: O(n)
+ *  Time Complexity: O(n log n)
+ *  Space Complexity: O(1)
+ * 
+ *   * Stuck Point:
+ *     (1) could not figure how to compare the ranges: even if brute force, how to compare?
+ *     (2) after finding that we can use sort, got wrong in comparing cases and updating merged meetings
+ * 
+ *  ** Untangled Hints: 
+ *     (1) use a simple example to understand what the problem is
+ *         : if needed, even draw the problem
+ *         -> shows how to approach the problem
+ *     (2) only need to compare WHETHER the ranges OVERLAP OR NOT
+ *         : ranges either overlap or not
+ *           -> overlap cases: a. (1, 3), (2, 3)
+ *                             b. (2, 3), (1, 3): same but need to compare differently (other direction from a)
+ *                             c. (1, 5), (2 ,3)
+ *                             d. (1, 5), (5, 7)
+ *           => can simplify overlapping cases by using SORT
+ *              : this way, can eliminate unnecessary comparing
  * 
  *  * What I learned:
+ *    
+ *    ** use a simple example to understand what the problem is
+ *      
+ *    ** how to MERGE RANGES
+ *       - first, SORT the ranges by start points in increasing order
+ *         -> this way, can get rid of work of comparing a start point to another start point
+ *       - then, check if (merged end point < new range's start point) to see if OVERLAPS or not
+ *         -> if overlaps, update the max end point between the two (cases: overlap, touch, include whole) 
+ *         -> else, insert new range since they are separate ranges (no overlap)
+ * 
+ *       => use SORT + OVERLAP condition checking
+ * 
+ *    3) be careful that you should UPDATE the values not include new ranges each time
+ * 
  */
 
 #include <bits/stdc++.h>
