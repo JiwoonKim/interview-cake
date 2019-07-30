cf7ace61f8a091e12b4b7ac2f798b8697bcfe9e1
JiwoonKim <diana0501@naver.com>
2019-07-30 17:38:16 +0900

    11-4. simulate 7 slided die 코드 및 필기 추가

--- 11_combination_probability_math/4_simulate_7_sided_die.cpp
+++ 11_combination_probability_math/4_simulate_7_sided_die.cpp
@@ -78,3 +78,27 @@ int rand7() {
     }
     return results[row][col];
 }
+
+/**
+ *  same solution
+ *  but w/o using memory to define results
+ *  and instead, using math
+*/
+int rand7() {
+    while (true) {
+        // do our die rolls
+        int roll1 = rand5();
+        int roll2 = rand5();
+
+        int outcomeNumber = (roll1 - 1) * 5 + (roll2 - 1) + 1;
+
+        // if we hit an extraneous
+        // outcome we just re-roll
+        if (outcomeNumber > 21) {
+            continue;
+        }
+
+        // our outcome was fine. return it!
+        return outcomeNumber % 7 + 1;
+    }
+}
